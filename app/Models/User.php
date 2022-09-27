1889479cefc63e1ad9712b3eefc9121f7acc5fac
dbasset <dbasset.oclock@mail.com>
2022-09-27 12:03:11 +0200

    Adding models + fillable

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -15,12 +17,20 @@ class User extends Authenticatable
     /**
      * The attributes that are mass assignable.
      *
-     * @var array<int, string>
+     * @var array<bool, string>
      */
     protected $fillable = [
         'name',
         'email',
         'password',
+        'email_verified_at',
+        'firstname',
+        'lastname',
+        'role_id',
+        'is_account_active',
+        'remember_token',
+        'created_at',
+        'updated_at',
     ];
 
     /**
@@ -41,4 +51,34 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function role(): BelongsTo
+    {
+        return $this->belongsTo(Role::class);
+    }
+
+    public function complaints(): HasMany
+    {
+        return $this->hasMany(Complaint::class);
+    }
+
+    public function passwordResets(): HasMany
+    {
+        return $this->hasMany(PasswordReset::class);
+    }
+
+    public function messages(): HasMany
+    {
+        return $this->hasMany(Message::class);
+    }
+
+    public function developer(): HasMany
+    {
+        return $this->hasMany(Developer::class);
+    }
+
+    public function orders(): HasMany
+    {
+        return $this->hasMany(Order::class);
+    }
 }
