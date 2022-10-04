ea4e27e787ce9f917df313226abefff5cf33544f
dbasset <dbasset.oclock@gmail.com>
2022-10-04 16:17:59 +0200

    Modify StackController

--- app/Http/Controllers/Api/Admin/StackController.php
+++ app/Http/Controllers/Api/Admin/StackController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Api\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\BackOffice\Stack\UpdateStackRequest;
-use App\Http\Requests\BackOffice\StoreStackRequest;
+use App\Http\Requests\BackOffice\Stack\StoreStackRequest;
 use App\Models\Stack;
 use Illuminate\Http\JsonResponse;
 
