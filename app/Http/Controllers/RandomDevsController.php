b325f3d8c91ef640ebef69e60b9d96e7b86159b0
ethan-eldib <ethan.eldib@gmail.com>
2022-10-11 11:53:48 +0200

    test seed

---
+++ app/Http/Controllers/RandomDevsController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\DB;
+
+class RandomDevsController extends Controller
+{
+    public function recoversSixRandomUsers(): \Illuminate\Http\JsonResponse
+    {
+        $developers = DB::select(
+            DB::raw('SELECT DISTINCT developers.*, (
+                                                    SELECT AVG(reviews.rating)
+                                                    FROM reviews
+                                                    WHERE reviews.dev_id = developers.id
+                                                  ) as avg
+                            FROM developers
+                            INNER JOIN reviews ON reviews.dev_id = developers.id
+                                WHERE (
+                                        SELECT COUNT(complaints.user_id)
+                                        FROM complaints
+                                        WHERE complaints.user_id = developers.user_id
+                                      ) = 0
+                                AND (
+                                        (
+                                            SELECT AVG(reviews.rating)
+                                            FROM reviews
+                                            WHERE reviews.dev_id = developers.id
+                                        ) >= 3
+                                        OR
+                                        (
+                                            SELECT COUNT(reviews.dev_id)
+                                            FROM reviews
+                                            WHERE reviews.dev_id = developers.id
+                                        ) = 0
+                                        OR
+                                        (
+                                            SELECT AVG(reviews.rating)
+                                            FROM reviews
+                                            WHERE reviews.dev_id = developers.id
+                                        ) = 0
+                                )
+            ')
+        );
+        $devs = [];
+        $old_keys = [];
+        if(sizeof($developers) >= 6){
+           while(sizeof($devs) < 6){
+               $key = rand(0,sizeof($developers)-1);
+               if(!in_array($key,$old_keys)){
+                   $devs[] = $developers[$key];
+               }
+               $old_keys[] = $key;
+           }
+        } else {
+            $devs = $developers;
+        }
+
+        return response()->json([
+            'developers' => $devs,
+        ], 200);
+    }
+}
