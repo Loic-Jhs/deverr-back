585cd90e4c267b136d96b66771131e65a9135db3
theo-hrd <theo.hrd02@gmail.com>
2022-10-17 19:22:52 +0200

    added is_account_parameter in request

--- app/Http/Controllers/Api/Developer/AllDevsController.php
+++ app/Http/Controllers/Api/Developer/AllDevsController.php
@@ -5,10 +5,15 @@ namespace App\Http\Controllers\Api\Developer;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\AllDevsResource;
 use App\Models\Developer;
+use Illuminate\Http\JsonResponse;
 
 class AllDevsController extends Controller
 {
-    public function getAllDevs()
+    /**
+     * Get all developers
+     * @return JsonResponse
+     */
+    public function getAllDevs(): \Illuminate\Http\JsonResponse
     {
         // get all developers id, firstname, lastname, created_at,
         // with their ratings summed, their stack, their devPrestations
@@ -16,10 +21,10 @@ class AllDevsController extends Controller
             ->get()
             ->filter(function ($developer) {
                 return $developer->developerStacks->count() > 0 &&
-                    $developer->developerPrestations->count() > 0;
+                    $developer->developerPrestations->count() > 0 &&
+                    $developer->user->is_account_active === 1;
             })
             ->shuffle();
-
 
         return response()->json(
         // On retourne les développeurs sous forme de ressources,
