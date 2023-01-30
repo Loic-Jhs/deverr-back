f3d17e697f4310b44897a443d45ebd03854bd925
theo-hrd <theo.hrd02@gmail.com>
2023-01-30 17:54:58 +0100

    formatted date

--- app/Http/Resources/OrdersResource.php
+++ app/Http/Resources/OrdersResource.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Carbon;
 
 class OrdersResource extends JsonResource
 {
@@ -17,7 +18,7 @@ class OrdersResource extends JsonResource
         return [
             'id' => $this->id,
             'user_fullname' => $this->user->lastname.' '.$this->user->firstname,
-            'created_at' => $this->created_at,
+            'created_at' => Carbon::parse($this->created_at)->format('d/m/Y'),
             'instructions' => $this->instructions,
             'is_paid' => $this->is_paid,
             'is_accepted_by_developer' => $this->is_accepted_by_developer,
